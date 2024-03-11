4440d9696441d8dd9704ce80929cd5444a238c1c
suminiee <chansu0114@naver.com>
2024-03-12 02:15:04 +0900

    Fix: spring security 로그인 오류 모두 수정&비밀번호 변경 로직 수정

--- src/main/java/com/medicare/neulpeum/service/UserServiceImpl.java
+++ src/main/java/com/medicare/neulpeum/service/UserServiceImpl.java
@@ -30,7 +30,8 @@ public class UserServiceImpl implements UserService{
             // 입력한 비밀번호를 암호화하여 유저 정보 찾아오기
             String encryptedCurrentPassword = passwordEncoder.encode(userPasswordUpdateRequestDto.getCurrentPassword());
             System.out.println(encryptedCurrentPassword);
-            Optional<UserInfo> optionalUserInfo = userRepository.findByPassword(encryptedCurrentPassword);
+            //currentPassword를 encoding하면 DB에 담겨있는 암호화된 비밀번호와 달라서 username으로 DB에서 찾아와야함
+            Optional<UserInfo> optionalUserInfo = userRepository.findByUsername("user");
             if (optionalUserInfo.isPresent()) {
                 UserInfo userInfo = optionalUserInfo.get();
                 // DB에서 가져온 암호화된 비밀번호와 입력한 암호화된 비밀번호 비교
@@ -44,7 +45,7 @@ public class UserServiceImpl implements UserService{
                     throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
                 }
             } else {
-                throw new IllegalArgumentException("비밀번호에 일치하는 유저가 존재하지 않습니다. 입력한 password : " + userPasswordUpdateRequestDto.getCurrentPassword());
+                throw new IllegalArgumentException("username이 user인 계정이 존재하지 않습니다.");
             }
         } catch (Exception e) {
             log.error("유저 비밀번호 변경 중 오류 발생 {}", e.getMessage());
@@ -55,20 +56,29 @@ public class UserServiceImpl implements UserService{
     @Override
     public void adminUpdate(AdminUpdateRequestDto adminUpdateRequestDto) {
         try {
-            Optional<UserInfo> optionalAdminInfo = userRepository.findByPassword(adminUpdateRequestDto.getCurrentPassword());
-            if (optionalAdminInfo.isPresent()) {
-                UserInfo userInfo = optionalAdminInfo.get();
-                //주어진 DTO에서 새로운 정보 추출하여 업데이트
-                userInfo.updatePassword(passwordEncoder, adminUpdateRequestDto.getNewPassword());
+            // 입력한 비밀번호를 암호화하여 유저 정보 찾아오기
+            String encryptedCurrentPassword = passwordEncoder.encode(adminUpdateRequestDto.getCurrentPassword());
+            System.out.println(encryptedCurrentPassword);
+            //currentPassword를 encoding하면 DB에 담겨있는 암호화된 비밀번호와 달라서 username으로 DB에서 찾아와야함
+            Optional<UserInfo> optionalUserInfo = userRepository.findByUsername("admin");
+            if (optionalUserInfo.isPresent()) {
+                UserInfo userInfo = optionalUserInfo.get();
+                // DB에서 가져온 암호화된 비밀번호와 입력한 암호화된 비밀번호 비교
+                if (passwordEncoder.matches(adminUpdateRequestDto.getCurrentPassword(), userInfo.getPassword())) {
+                    // 주어진 DTO에서 새로운 비밀번호 추출하여 업데이트
+                    userInfo.updatePassword(passwordEncoder, adminUpdateRequestDto.getNewPassword());
 
-                //업데이트 된 정보 저장
-                userRepository.save(userInfo);
+                    // 업데이트 된 정보 저장
+                    userRepository.save(userInfo);
+                } else {
+                    throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
+                }
             } else {
-                throw new IllegalArgumentException("입력한 비밀번호에 일치하는 관리자 정보를 찾을 수 없습니다. 입력한 password : " + adminUpdateRequestDto.getCurrentPassword());
+                throw new IllegalArgumentException("username이 admin인 계정이 존재하지 않습니다.");
             }
         } catch (Exception e) {
             log.error("관리자 비밀번호 변경 중 오류 발생 {}", e.getMessage());
-            throw new RuntimeException("관리자 비밀번호 변경 중 오류 발생" + e.getMessage());
+            throw new RuntimeException("관리자 비밀번호 변경 중 오류 발생 " + e.getMessage());
         }
     }
 }
