f9567bf3d38bd3ed46f59c20624ebc3a4d32aa29
suminiee <chansu0114@naver.com>
2024-08-20 15:28:52 +0900

    Fix: MVC allowedOrigins 링크 추가

--- src/main/java/com/medicare/neulpeum/config/WebMvcConfig.java
+++ src/main/java/com/medicare/neulpeum/config/WebMvcConfig.java
@@ -10,7 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000",
-                        "http://0.0.0.0:3000")
+                        "http://0.0.0.0:3000",
+                        "https://d14yvif52hqxqx.cloudfront.net/")
                 .allowedMethods("GET","POST","PUT","DELETE", "PATCH")
                 .allowCredentials(true);
 
